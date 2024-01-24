2cc52fdc1f4d6cadd5c5b12933f7b247c120cef2
harpreet <harpreetx01@outlook.com>
2024-01-23 17:36:14 -0800

    backend accessed the firebase auth with the admin (provided the private key) also setup the login route which decode the given token with admin access

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
+const admin = require("../config/firebase");
 
 // for the login
-router.get("/login", (req, res) => {
-  return res.json("login");
+// getting the token and validating and decoding it with the help of the admin which is configured in config file and also it has the private key to access it
+router.get("/login", async (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(500).send({ message: "invalid token" });
+  }
+  const token = req.headers.authorization.split("")[1];
+  const decodeValue = await admin.auth().verifyIdToken(token);
+  if (!decodeValue) {
+    return res.status(505).json({ message: "unauthorised" });
+  } else return res.status(200).json(decodeValue);
 });
 module.exports = router;

5dda99c6698dfdcea8615daf3a6be6e9d2cb1ed4
harpreet <harpreetx01@outlook.com>
2024-01-24 00:37:02 -0800

    schema for the user has been setup to be stored in the mongodb

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const admin = require("../config/firebase");
+const user = require("../models/user");
 
 // for the login
 // getting the token and validating and decoding it with the help of the admin which is configured in config file and also it has the private key to access it
@@ -11,6 +12,30 @@ router.get("/login", async (req, res) => {
   const decodeValue = await admin.auth().verifyIdToken(token);
   if (!decodeValue) {
     return res.status(505).json({ message: "unauthorised" });
-  } else return res.status(200).json(decodeValue);
+  } else {
+    // checking user exist or not
+    const userExists = await user.findOne({ user_id: decodeValue.user_id });
+    if (!userExists) {
+      newUserData(decodeValue, req, res);
+    } else return res.send("need to updated");
+  }
 });
+//creating the new user data
+const newUserData = async (decodeValue, req, res) => {
+  const newUser = new user({
+    name: decodeValue.name,
+    email: decodeValue.email,
+    imageURL: decodeValue.picture,
+    user_id: decodeValue.user_id,
+    email_verified: decodeValue.email_verified,
+    role: "member",
+    auth_time: decodeValue.auth_time,
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(200).send({ user: savedUser });
+  } catch (error) {
+    res.status(400).send({ success: false, msg: error });
+  }
+};
 module.exports = router;
